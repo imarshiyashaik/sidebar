50891a9f2ae3b75e5126eded783cbfd62226739e
Shaik Mahaboob Arshiya <shaik.arshiya@HGTL.COM>
2024-11-12 10:49:05 +0530

    inital commit

---
+++ src/Component/Sidebar/Sidebar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './Sidebar.css'
+import { SidebarData } from '../SidebarData';
+
+export default function Sidebar(){
+    const[isOpen, setIsOpen] = useState(true);
+    const toggleSidebar = () =>{
+        setIsOpen(!isOpen)
+    }
+    return(
+        <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+            <div className='sidebar-header'>
+                <button className='toggle-btn' onClick={toggleSidebar}>
+                    <div className={`arrow ${isOpen ? 'left' : 'right'}`}></div>
+                </button>
+                <div className='logo'>
+                    <h2> {isOpen? "My-App" : ""}</h2>
+
+                </div>
+            </div>
+            <nav class='nav-menu'>
+                <ul>
+                    {SidebarData?.map((item, index) =>(
+                        <li key={index}>
+                            <a href={item.path}>
+                                {item.icon}
+                                {isOpen? item.title: ""}
+                            </a>
+                        </li>
+
+                    ))}
+                </ul>
+
+            </nav>
+        </div>
+    )
+}
---
+++ src/Component/SidebarData.js
@@ -0,0 +1,18 @@
+// import HomeIcon from '@mui/icons-material/Home';
+export const SidebarData = [
+    {
+      title: 'Home',
+      icon: "",
+      path:"#home"  
+    },
+    {
+        title: 'Services',
+        icon: "",
+        path:"#services"  
+      },
+      {
+        title: 'About',
+        icon: "",
+        path:"#about"  
+      },
+    ]
